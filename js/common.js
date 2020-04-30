d4e3efaf23bff90ba4919ef95cae23c7e8e65fb7
ukabyl <umkabylbekov@gmail.com>
2020-04-30 20:26:16 +0500

    Navigation bar is built dynamically

--- js/common.js
+++ js/common.js
@@ -1,7 +1,5 @@
 // section elements
 const sections = document.querySelectorAll('.section');
-//links
-const links = document.querySelectorAll('.nav-list__link');
 
 // Create a scroll function
 function smoothScroll(selector, duration) {
@@ -22,38 +20,61 @@ function smoothScroll(selector, duration) {
   // Mathematical function from gizma.com for easing animation
   function ease(t, b, c, d) {
     t /= d/2;
-  	if (t < 1) return c/2*t*t*t*t*t + b;
-  	t -= 2;
-  	return c/2*(t*t*t*t*t + 2) + b;
+    if (t < 1) return c/2*t*t*t*t*t + b;
+    t -= 2;
+    return c/2*(t*t*t*t*t + 2) + b;
   }
 
   requestAnimationFrame(animation);
 
 }
 
-// Add event listener to each link
-links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    smoothScroll('.' + e.currentTarget.dataset.link, 3000);
-  })
-});
+document.addEventListener('DOMContentLoaded', () => {
+  // Appending elements to the DOM
+  document.querySelector('.nav-list').innerHTML = `
+        <li class="nav-list__item">
+          <a data-link="section1" class="nav-list__link active">Main</a>
+        </li>
+        <li class="nav-list__item">
+          <a data-link="section2" class="nav-list__link">About</a>
+        </li>
+        <li class="nav-list__item">
+          <a data-link="section3" class="nav-list__link">Portfolio</a>
+        </li>
+        <li class="nav-list__item">
+          <a data-link="section4" class="nav-list__link">Section 1</a>
+        </li>
+        <li class="nav-list__item">
+          <a data-link="section5" class="nav-list__link">Section 2</a>
+        </li>
+    `;
 
-let nextScroll = false;
+    //links
+    const links = document.querySelectorAll('.nav-list__link');
 
-window.addEventListener('scroll', (e) => {
-  nextScroll = true;
-  const timeout = setTimeout(() => {
-    nextScroll = false;
-    sections.forEach((section) => {
-      const top = section.offsetTop - 10;
-      const bottom = top + parseInt(window.getComputedStyle(section, null).height) - 200;
-      const scroll = window.scrollY;
-      const selector = section.classList[1];
-      if( scroll > top && scroll < bottom){
-            links.forEach((link) => link.classList.remove('active'));
-            document.querySelector(`a[data-link="${selector}"]`).classList.add('active');
-        }
+    // Add event listener to each link
+    links.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        smoothScroll('.' + e.currentTarget.dataset.link, 3000);
+      })
     });
-  }, 50);
-  setTimeout(() => { if (nextScroll) clearTimeout(timeout) }, 50);
+
+    let nextScroll = false;
+    window.addEventListener('scroll', (e) => {
+      nextScroll = true;
+      const timeout = setTimeout(() => {
+        nextScroll = false;
+        sections.forEach((section) => {
+          const top = section.offsetTop - 10;
+          const bottom = top + parseInt(window.getComputedStyle(section, null).height) - 200;
+          const scroll = window.scrollY;
+          const selector = section.classList[1];
+          if( scroll > top && scroll < bottom){
+                links.forEach((link) => link.classList.remove('active'));
+                document.querySelector(`a[data-link="${selector}"]`).classList.add('active');
+            }
+        });
+      }, 50);
+      setTimeout(() => { if (nextScroll) clearTimeout(timeout) }, 50);
+    })
 })
